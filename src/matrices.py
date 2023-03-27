1bff47e9f64bdcd0c77a6f859de7a1aaf92878ff
MattD <mdd423@nyu.edu>
2023-03-27 19:12:58 -0400

    import neuron equations from src

--- src/matrices.py
+++ src/matrices.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.sparse as sparse
 
+from dataclasses import dataclass
+
 class RandomMatDist:
     def __init__(self,f,u):
         self.f = f
@@ -51,3 +53,78 @@ def get_J_matrix(N,u,w,g=0.0,f=1.0,alpha=1.0,beta=1.0,gamma=1.0):
     # inhb[inds,inds] = 0.0
 
     return neig + idnt - inhb
+
+paper_parameters = [{'u'    :0.5
+                      ,'w'    :0.0
+                      ,'alpha':1.0
+                      ,'beta' :0.0
+                      ,'gamma':0.0}
+
+                     ,{'u'    :0.0
+                      ,'w'    :0.0
+                      ,'alpha':1.0
+                      ,'beta' :1.0
+                      ,'gamma':1.0}
+
+                     ,{'u'    :0.5
+                      ,'w'    :0.0
+                      ,'alpha':1.0
+                      ,'beta' :1.0
+                      ,'gamma':1.0}
+
+                     ,{'u'    :0.5
+                      ,'w'    :0.5
+                      ,'alpha':1.0
+                      ,'beta' :1.0
+                      ,'gamma':1.0}]
+
+@dataclass
+class NeuronDiffEq:
+    matrix: np.matrix
+    tau: np.double
+    N: int
+
+    def activation(self,x):
+        '''
+            Activation function on neurons that take sum of inputs
+        '''
+        out = np.zeros(x.shape)
+        out[(x + 1) < 0]  = 0.0
+        out[(x + 1) >= 0] = x[(x + 1) >= 0] + 1
+        return out
+
+    def external(self,t):
+        '''
+            External input to each neuron at time, t
+        '''
+
+        return 0.0
+
+    def __call__(self,r,t):
+        '''
+            THE EQUATION
+        '''
+        interactions = np.array(self.matrix @ r).squeeze()
+        out = (-r + self.activation(interactions + self.external(t)).squeeze())/self.tau
+        return out
+
+@dataclass
+class NoisyNeuron(NeuronDiffEq):
+    mu: np.double
+    sigma: np.double
+
+    def external(self,t):
+
+        out = np.random.normal(self.mu,self.sigma,self.N)
+        return out
+
+@dataclass
+class ConstantInput(NeuronDiffEq):
+    constant: np.double
+    sites: slice
+
+    def external(self,t):
+
+        out = np.zeros(self.N)
+        out[self.sites] = self.constant
+        return out
